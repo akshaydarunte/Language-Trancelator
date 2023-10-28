cef8050181b9842d7cae764a4207dd6ae19ab490
Akshay Darunte <94286993+akshaydarunte@users.noreply.github.com>
2023-10-28 15:49:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,23 @@ def translate_sentence(sentence, target_language):
 
 def detect_language(sentence):
     translator = Translator()
-    detected_language = translator.detect(sentence).lang
+    try:
+        detected_language = translator.detect(sentence).lang
+    except AttributeError:
+        return "Unable to detect language"
     return detected_language
+
+# Add a background color gradient with sky blue effect
+st.markdown(
+    """
+    <style>
+    body {
+        background-image: linear-gradient(to bottom, #87CEEB, #00688B);
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 st.title("Language Translation App")
 
@@ -18,7 +33,10 @@ target_language = st.selectbox("Select a target language:", list(LANGUAGES.value
 
 if st.button("Translate"):
     detected_language = detect_language(input_sentence)
-    st.write(f"Detected Language: {LANGUAGES[detected_language]}")
+    if detected_language == "Unable to detect language":
+        st.write("Language detection failed. Please check your input or try specifying the language.")
+    else:
+        st.write(f"Detected Language: {LANGUAGES[detected_language]}")
     
     translated_sentence = translate_sentence(input_sentence, target_language)
     st.write(f"Translated sentence: {translated_sentence}")
